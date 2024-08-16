c6073388b05427a89b0b93386c31cbad69fb741f
deaxparadox <nitish136900kushwaha@gmail.com>
2024-08-16 12:36:41 +0530

    setting up jenkins

--- back/myprofile/admin.py
+++ back/myprofile/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Profile, Setting, Account
+
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+class SettingAdmin(admin.ModelAdmin):
+    pass 
+
+class AccountAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Setting, SettingAdmin)
+admin.site.register(Account, AccountAdmin)
--- back/myprofile/models.py
+++ back/myprofile/models.py
@@ -15,32 +15,65 @@ def phone_number_validator(value):
             params={"value": value},
         )
         
-# def email_validator(value)
+
 
 # Create your models here.
 class Profile(models.Model):
-    username = models.CharField("User username", max_length=120, unique=True, primary_key=True)
     first_name = models.CharField("User firstname", max_length=120, blank=True, null=True)
     last_name = models.CharField("User lastname", max_length=120, blank=True, null=True)
-    phone = models.IntegerField("User phone number", validators=[phone_number_validator])
-    date_of_birth = models.DateField("User date of birth")
-    email = models.EmailField("User email", max_length=255)
-    password = models.TextField("User password")
+    phone = models.IntegerField("User phone number", validators=[phone_number_validator], null=True, blank=True)
+    date_of_birth = models.DateField("User date of birth", blank=True, null=True)
     
     def __repr__(self) -> str:
-        return "%s -> (%s %s)" % (self.username, self.first_name, self.last_name)
+        return "Name -> [%s %s]" % (self.first_name, self.last_name)
     
     def __str__(self) -> str:
         return repr(self)
-
-
-class FBUser(models.Model):
-    profile = models.OneToOneField(Profile, models.SET_NULL, related_name="fbuser")
-    friends = models.ManyToManyField(Profile)
     
+    
+class ProfileViewChoices(models.TextChoices):
+    LOCKED = "LKD", _("Locked")
+    UNLOCKED = "UKD", _("UnLocked")
+    
+class ProfileStatusChoices(models.TextChoices):
+    ACTIVE = "ACT", _("Active")
+    DEACTIVE = "DCT", _("Deactive")
+    
+class ProfileDeleteChoices(models.TextChoices):
+    YES = "Y", _("Yes")
+    NO = "N", _("No")
+    
+    
+class Setting(models.Model):
+    locked = models.CharField(
+        max_length=3,
+        choices=ProfileViewChoices,
+        default=ProfileViewChoices.UNLOCKED
+    )
+    status = models.CharField(
+        max_length=3, 
+        choices=ProfileStatusChoices, 
+        default=ProfileStatusChoices.ACTIVE
+    )
+    delete = models.CharField(
+        max_length=1,
+        choices=ProfileDeleteChoices,
+        default=ProfileDeleteChoices.NO
+    )
+
+
+class Account(models.Model):
+    username = models.CharField("User username", max_length=120, unique=True, primary_key=True)
+    password = models.TextField("User password", max_length=24)
+    email = models.EmailField("User email", max_length=255)
+    friends = models.ManyToManyField("self")
+    
+    
+    profile = models.OneToOneField(Profile, on_delete=models.SET_NULL, blank=True, null=True)
+    setting = models.OneToOneField(Setting, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __repr__(self) -> str:
-        return self.profile
+        return "[%s]" % self.username
     
     def __str__(self) -> str:
         return repr(self)
